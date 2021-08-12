0d258757b72a41cc8cdca57931d97816699cbce7
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-08-11 22:17:11 -0400

    fix commit

--- src/app/components/section-card/section-card.component.ts
+++ src/app/components/section-card/section-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-card',
   templateUrl: './section-card.component.html',
-  styleUrls: ['./section-card.component.scss']
+  styleUrls: ['./section-card.component.scss'],
 })
 export class SectionCardComponent implements OnInit {
+  @Input() sectionNumber: string = '';
+  @Input() sectionTitle: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
